32d70a64d5c2bd41667daaacd580145c7500fe40
Diego Alves de Souza <diegosouza.mail@gmail.com>
2012-11-03 23:25:16 -0200

    Fourth Level Complete

--- diego-beginner/player.rb
+++ diego-beginner/player.rb
@@ -1,13 +1,26 @@
 class Player
   def play_turn(warrior)
+    @warrior = warrior
+    @previous_health ||= 20
     if warrior.feel.empty?
-      if warrior.health < 20
-        warrior.rest!
+      if under_attack?
+        @warrior.walk!
+      elsif !full_health?
+        @warrior.rest!
       else
-        warrior.walk!
+        @warrior.walk!
       end
     else
-      warrior.attack!
+      @warrior.attack!
     end
+    @previous_health = @warrior.health
+  end
+
+  def full_health?
+    @warrior.health == 20
+  end
+
+  def under_attack?
+    @warrior.health < @previous_health
   end
 end
