0ec8b62774db95ef236aec238b397cc6da45c3b6
Diego Alves de Souza <diegosouza.mail@gmail.com>
2012-11-04 02:56:05 -0200

    Several Score improvements by not resting and walking away when there's no more enemies

--- diego-beginner/player.rb
+++ diego-beginner/player.rb
@@ -4,12 +4,14 @@ class Player
     @warrior = warrior
     @direction ||= has_captives_behind? ? :backward : :forward
     @previous_health ||= 20
+
     if warrior.feel(@direction).empty?
       if under_attack? && dying?
         @warrior.walk!(:backward)
-      elsif !under_attack? && can_shoot?
+        @enemies_left = true
+      elsif can_shoot?
         @warrior.shoot!
-      elsif under_attack? || full_health?
+      elsif under_attack? || full_health? || !enemies_left?
         @warrior.walk!(@direction)
       else
         @warrior.rest!
@@ -31,19 +33,52 @@ class Player
     @warrior.health == 20
   end
 
+  def enemies_left?
+    vision = @warrior.look
+    vision.delete_if {|s| !s.enemy?}
+    @enemies_left || vision.any?
+  end
+
+  def targeted?
+    vision = @warrior.look
+    vision.delete_if {|s| !s.enemy?}
+    if vision.any?
+      case vision.first.unit.name
+      when "Wizard"
+        true
+      when "Archer"
+        true
+      else
+        false
+      end
+    else
+      false
+    end
+  end
+
   def under_attack?
     @warrior.health < @previous_health
   end
 
   def dying?
-    @warrior.health < 7
+    @warrior.health < 5
   end
 
   def can_shoot?
     vision = @warrior.look
-    vision.delete_if {|s| s.empty?}
-    vision.size > 0 && vision.first.unit.class == RubyWarrior::Units::Wizard
-    #vision.size > 0 && vision.first.enemy?
+    vision.delete_if {|s| s.empty? || s.stairs? || s.wall?}
+    if vision.any?
+      case vision.first.unit.name
+      when "Wizard"
+        true
+      when "Archer"
+        dying? #&& targeted?
+      else
+        false
+      end
+    else
+      false
+    end
   end
 
   def has_captives_behind?
